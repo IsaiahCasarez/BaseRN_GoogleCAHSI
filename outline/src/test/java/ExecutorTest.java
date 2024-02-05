e94ab7cb1095ac98c786bbdbd990f69d743df1f1
John Pateros <jpateros5410@sdsu.edu>
2024-02-05 13:34:01 -0800

    iniial file structure

---
+++ outline/src/test/java/ExecutorTest.java
@@ -0,0 +1,9 @@
+
+public class ExecutorTest {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+
+    }
+}
--- outline/src/test/java/PlannerTest.java
+++ outline/src/test/java/PlannerTest.java
@@ -1,5 +1,5 @@
 
-public class Parser {
+public class PlannerTest {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
